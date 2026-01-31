50d611f9bc587232299fe1b29b78bdea1ddfb4f4
2767277092 <2767277092@qq.com>
2026-01-31 19:28:30 +0800

    1-31-4

---
+++ python-project01/第二章/12函数02.py
@@ -0,0 +1,11 @@
+#需求3:完成如下列表的排序操作,按照每一个元素的字符个数从小到大排序
+data_list=["C++","C","Python","Java","PHP","JAVA"]
+for j in (0,len(data_list)-1):
+    for i in range(1,len(data_list)):
+        if len(data_list[i])<=len(data_list[i-1]):
+            pop=data_list[i-1]
+            data_list[i-1]=data_list[i]
+            data_list[i]=pop
+print(data_list)
+
+#list.sort是按照编码排序
